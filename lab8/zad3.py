2b892ba621f50f4cb95b7ab6f46476f9685e06fd
kkolodziejski <kolodziejskikrzysztofhubert@gmail.com>
2022-05-17 21:11:51 +0200

    lab8

--- lab8/zad3.py
+++ lab8/zad3.py
@@ -2,11 +2,21 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 import seaborn as sns
 
+scaler = StandardScaler()
 sns.set()
 
-df = pd.read_csv("iris.csv")
+data = pd.read_csv("iris.csv")
+
+target = data[['class']].replace(['setosa','versicolor','virginica'],[0,1,2])
+
+df_norm = data[['sepallength','sepalwidth','petallength','petalwidth']].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+df = pd.concat([df_norm, target], axis=1)
 (train_set, test_set) = train_test_split(df.values, train_size=0.7, random_state=33)
 
 X_test = []
@@ -21,22 +31,41 @@ for i in train_set:
     X_train.append([i[0], i[1], i[2], i[3]])
     y_train.append(i[4])
 
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(2, 1), random_state=1)
-clf.fit(X_train, y_train)
-prediction = clf.predict(X_test)
+print("4-2-1")
 
-print("Accuraccy: ", metrics.accuracy_score(prediction, y_test) * 100, "%")
+mlp = MLPClassifier(hidden_layer_sizes=(2), max_iter=5000)
+mlp.fit(X_train, y_train)
 
+predictions_train = mlp.predict(X_train)
+print(accuracy_score(predictions_train, y_train))
+predictions_test = mlp.predict(X_test)
+print(accuracy_score(predictions_test, y_test))
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(3, 1), random_state=1)
-clf.fit(X_train, y_train)
-prediction = clf.predict(X_test)
+print("4-3-1")
 
-print("Accuraccy: ", metrics.accuracy_score(prediction, y_test) * 100, "%")
+mlp = MLPClassifier(hidden_layer_sizes=(3), max_iter=5000)
+mlp.fit(X_train, y_train)
 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(3, 3, 1), random_state=1)
-clf.fit(X_train, y_train)
-prediction = clf.predict(X_test)
+predictions_train = mlp.predict(X_train)
+print(accuracy_score(predictions_train, y_train))
+predictions_test = mlp.predict(X_test)
+print(accuracy_score(predictions_test, y_test))
 
-print("Accuraccy: ", metrics.accuracy_score(prediction, y_test) * 100, "%")
+print("4-3-3-1")
+
+mlp = MLPClassifier(hidden_layer_sizes=(3,3), max_iter=5000)
+mlp.fit(X_train, y_train)
+
+predictions_train = mlp.predict(X_train)
+print(accuracy_score(predictions_train, y_train))
+predictions_test = mlp.predict(X_test)
+print(accuracy_score(predictions_test, y_test))
+
+# print(confusion_matrix(predictions_train, y_train))
+#
+# print(confusion_matrix(predictions_test, y_test))
+
