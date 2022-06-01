b8188766e303bd340b18137df38f2fd2b1d2bf1c
kkolodziejski <kolodziejskikrzysztofhubert@gmail.com>
2022-06-01 15:35:18 +0200

    chopping scenario into character lines

--- projekt2/textPrepare.py
+++ projekt2/textPrepare.py
@@ -1,16 +1,13 @@
 data = []
 
-# LISA
-# JOHNNY
-# MARK
-lisa = 0
-johnny = 0
-mark = 0
 dialogTest = 0
 
 lisaSentences = ''
 johnnySentences = ''
 markSentences = ''
+claudetteSentences = ''
+michelleSentences = ''
+peterSentences = ''
 
 scenarioLines = []
 
@@ -21,42 +18,35 @@ with open("TheRoom.txt", "r") as scenario:
 # print(len(scenarioLines))
 iterator = 0
 
-while iterator < len(scenarioLines):
-    if scenarioLines[iterator] == "LISA":
-        lisa = lisa + 1
-        lineChecker = 0
-        endOfStatement = False
-        while true:
-            if ()
+
+def getPersonLines(name):
+    global dialogTest
+    personSentences = ''
+    if scenarioLines[iterator] == name:
+        lineIterator = iterator + 1
+        while True:
+            if "(" in scenarioLines[lineIterator] or ")" in scenarioLines[lineIterator]:
+                lineIterator = lineIterator + 1
+            elif scenarioLines[lineIterator][0].isupper() and scenarioLines[lineIterator][1].isupper() and \
+                    scenarioLines[lineIterator][2].isupper():
+                break
+            else:
+                personSentences = personSentences + " " + scenarioLines[lineIterator]
+                lineIterator = lineIterator + 1
+                dialogTest = dialogTest + 1
+    return personSentences
 
 
-        if scenarioLines[iterator + 1][0].isupper() and scenarioLines[iterator + 1][1].islower():
-            dialogTest = dialogTest + 1
-    if scenarioLines[iterator] == "JOHNNY":
-        johnny = johnny + 1
-    if scenarioLines[iterator] == "MARK":
-        mark = mark + 1
+while iterator < len(scenarioLines) - 8:
+    lisaSentences = lisaSentences + getPersonLines("LISA")
+    johnnySentences = johnnySentences + getPersonLines("JOHNNY")
+    markSentences = markSentences + getPersonLines("MARK")
+    claudetteSentences = claudetteSentences + getPersonLines("CLAUDETTE")
+    michelleSentences = michelleSentences + getPersonLines("MICHELLE")
+    peterSentences = peterSentences + getPersonLines("PETER")
     iterator = iterator + 1
 
-
-# with open("TheRoom.txt", "r") as scenario:
-#     for i in scenario:
-#
-#         if (i == "LISA\n"):
-#             lisa = lisa + 1
-#             if (scenario[iterator][0].isupper() and scenario[iterator][1].islower()):
-#
-#
-#         if (i == "JOHNNY\n"):
-#             johnny = johnny + 1
-#         if (i == "MARK\n"):
-#             mark = mark + 1
-#         iterator = iterator + 1
-#         # print(i)
-#         # data.append(i.replace('\n', ''))
-#
-#
-print(lisa)
-print(johnny)
-print(mark)
+# print(lisaSentences)
+# print(johnnySentences)
+# print(markSentences)
 print(dialogTest)
